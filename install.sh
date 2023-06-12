b1258e14dc54d34d67da65a0e11b99bbec202257
Koinu <serika@milkdog.onmicrosoft.com>
2023-06-12 11:15:11 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+wget -P /tmp "https://github.com/KoinuDayo/Xray-geodat-update/raw/main/xray-dat-update.service"
+wget -P /tmp "https://github.com/KoinuDayo/Xray-geodat-update/raw/main/xray-dat-update.timer"
+wget -P /tmp "https://github.com/KoinuDayo/Xray-geodat-update/raw/main/updategeodat.sh"
+
+mv /tmp/updategeodate.sh /usr/local/bin/updategeodat.sh
+mv /tmp/xray-dat-update.service /etc/systemd/system/xray-dat-update.service
+mv /tmp/xray-dat-update.timer /etc/systemd/system/xray-dat-update.timer
+
+echo -e "Install complete, but you may need enable service and timer by your self.\nsystemctl enable xray-dat-update.service\nsystemctl enable xray-dat-update.timer"
